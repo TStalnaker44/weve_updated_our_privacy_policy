d27c68a7fc5d5ddbc054592331df54d6c78b1b68
TStalnaker44 <28265728+TStalnaker44@users.noreply.github.com>
2022-02-15 13:37:51 -0500

    Created python utility script

---
+++ md2html.py
@@ -0,0 +1,15 @@
+
+"""
+This file converts markdown files (.md) into HTML.  This conversion
+will be convenient when we want to display privacy policies in our
+document viewer.  The HTML versions available on GitHub are not strictly
+HTML, but rather include the javascript required to load the page.
+That is that some of the html files appear to be proceedurally generated, 
+which could present an issue in the future.
+
+NOTE: In order to run this file, you will need to install the
+markdown module by typing the following command into your terminal:
+
+pip install markdown
+
+"""
