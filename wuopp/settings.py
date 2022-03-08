b4f3d0bf131fd7d8196afd8364babc0b0abf078f
nwintersgill <natewintersgill@yahoo.com>
2022-03-08 15:18:11 -0500

    structure, protect secret key

--- wuopp/settings.py
+++ wuopp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vk-f02cuvyd^i^m36^6e-zd^2%qgaa5%qk_=pkrq#^ez8ow%t*'
+SECRET_KEY = config("SECRET_KEY") # requires .env file: see https://stackoverflow.com/questions/64208678/hiding-secret-key-in-django-project-on-github-after-uploading-project
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
